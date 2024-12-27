e8e086512ce5ebdb4c08be6f3fae20414c6e0655
schakibb <schakibhad@gmail.com>
2024-12-27 06:10:47 +0100

    feat: users structure added in data + read users json's file

--- quiz/data_handler.py
+++ quiz/data_handler.py
@@ -13,3 +13,8 @@ def write_users(users):
     with open(USER_DATA_FILE, "w") as file:
         json.dump(users, file, indent=4)
 
+def read_questions(subject):
+    if not os.path.exists(os.path.join(os.path.dirname(__file__), "../data/"+subject+"Qs.json")):
+        return []
+    with open(os.path.join(os.path.dirname(__file__), "../data/"+subject+"Qs.json"), "r") as file:
+        return json.load(file)
