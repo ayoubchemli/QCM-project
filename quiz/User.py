c7faa8ec838eafa2be13eff634afa2b5a76d53a0
unknown <imadbouchama404@gmail.com>
2024-12-28 22:22:45 +0100

    export the score of a user #9

--- quiz/User.py
+++ quiz/User.py
@@ -1,4 +1,7 @@
 from Score import Score
+import csv
+from typing import List
+
 class User:
     def __init__(self, fullname, email, username, password, scores=None):
         if scores is None:
@@ -28,6 +31,53 @@ class User:
         scores = [Score(score_data['subject'], score_data['points']) for score_data in
                   data['scores']]
         return User(data['fullName'], data['email'], data['username'], data['password'], scores)
+    
+
+    @staticmethod
+    def export_users_to_csv(users: List['User'], filename: str) -> None:
+        """
+        Export a list of User objects to a CSV file with separate columns for each score.
+        
+        Args:
+            users: List of User objects to export
+            filename: Name of the CSV file to create
+        """
+        
+        subjects = set()
+        for user in users:
+            for score in user.scores:
+                subjects.add(score.subject)
+        subjects = sorted(list(subjects))
+        
+        
+        headers = ['Full Name', 'Email', 'Username'] + subjects
+        
+        
+        with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile)
+            
+            
+            writer.writerow(headers)
+            
+            
+            for user in users:
+                
+                score_dict = {score.subject: score.points for score in user.scores}
+                
+            
+                row = [
+                    user.fullname,
+                    user.email,
+                    user.username
+                ]
+                
+                
+                for subject in subjects:
+                    row.append(score_dict.get(subject, ''))
+                
+                writer.writerow(row)
+
+
 
 
     def add_scores(self, subject,points):
