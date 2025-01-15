d614c557a6fd7e7917ec346ea6f8a7b810be8c8c
Adam-Belkadi <adambelkadi1@gmail.com>
2025-01-15 18:11:53 +0100

    fixed the gitignore

--- quiz/__init__.py
+++
@@ -1,4 +0,0 @@
-# from .login_register import authenticate_user, add_user
-# from .data_handler import read_users, write_users
-
-# __all__ = ["authenticate_user", "add_user", "read_users", "write_users"]
